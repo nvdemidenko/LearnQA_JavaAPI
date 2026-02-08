acd277f5ef7b291aa849bf2ef91f0f00be4fd4ee
nvdemidenko <n.v.demidenko@gmail.com>
2026-02-08 21:27:53 +0300

    hello world from Nalalya test

---
+++ src/test/java/HelloWorldFromNatalyaTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldFromNatalyaTest {
+    @Test
+    public void HelloWorldFromNatalyaTest(){
+       System.out.println("Hello from Natalya");
+    }
+}
