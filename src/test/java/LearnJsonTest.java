d4dae9b6fcbd6b5a93d8bd0c8166cfd324717925
nvdemidenko <n.v.demidenko@gmail.com>
2026-02-10 22:12:39 +0300

    add TestJsonRestAssured

--- src/test/java/LearnJsonTest.java
+++ src/test/java/LearnJsonTest.java
@@ -2,10 +2,19 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-public class HelloWorldTest {
+import java.util.HashMap;
+import java.util.Map;
+
+public class LearnJsonTest {
+
     @Test
-    public void TestRestAssured(){
+    public void TestJsonRestAssured(){
+        Map<String,String> params = new HashMap<>();
+        params.put("name", "john");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
