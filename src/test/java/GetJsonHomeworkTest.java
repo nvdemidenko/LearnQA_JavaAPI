8c32ade03586875802c64e6fb17d07b691def017
nvdemidenko <n.v.demidenko@gmail.com>
2026-02-11 00:04:54 +0300

    add show GetJsonHomeworkTest

---
+++ src/test/java/GetJsonHomeworkTest.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class GetJsonHomeworkTest {
+
+    @Test
+    public void TestGetJsonHomework(){
+
+       JsonPath response = RestAssured
+               .when()
+               .get("https://playground.learnqa.ru/api/get_json_homework")
+               .then()
+               .extract()
+               .jsonPath();
+
+        System.out.print("Второе сообщение: ");
+        System.out.println(response.getString("messages[1].message"));
+    }
+}
