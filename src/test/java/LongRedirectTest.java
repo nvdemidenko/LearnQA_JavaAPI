79dc2172bbec8d6979d74820c3680f8cd66c99bd
nvdemidenko <n.v.demidenko@gmail.com>
2026-02-11 21:49:21 +0300

    add Redirect Until Code 200 Test

--- src/test/java/LongRedirectTest.java
+++ src/test/java/LongRedirectTest.java
@@ -2,16 +2,11 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class LongRedirectTest {
 
     @Test
     public void TestLongRedirect(){
-        Map<String, String> headers = new HashMap<>();
-        headers.put("myHeader1","value1");
-        headers.put("myHeader2","value2");
 
         Response response = RestAssured
                 .given()
--- src/test/java/RedirectUntilCode200Test.java
+++ src/test/java/RedirectUntilCode200Test.java
@@ -2,22 +2,30 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class RedirectUntilCode200Test {
 
     @Test
-    public void TestLongRedirect(){
+    public void TestLongRedirect() {
 
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .get("https://playground.learnqa.ru/api/long_redirect")
-                .andReturn();
-
-        String locationHeader = response.getHeader("Location");
-        System.out.println(locationHeader);
+        int statusCode = 0;
+        int i = 0;
+        String redirectUrl = "https://playground.learnqa.ru/api/long_redirect";
+        while (redirectUrl != null) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .get(redirectUrl)
+                    .andReturn();
+            statusCode = response.getStatusCode();
+            //System.out.println(statusCode);
+            if (statusCode == 200) {
+                break;
+            }
+            redirectUrl = response.getHeader("Location");
+            i++;
+        }
+        System.out.print("Количество попыток: ");
+        System.out.println(i);
     }
 }
