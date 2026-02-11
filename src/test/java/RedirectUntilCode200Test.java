dffbeb2f52918a94db582ff4de9ab26a694e06f8
nvdemidenko <n.v.demidenko@gmail.com>
2026-02-11 21:07:38 +0300

    add redirect text test

---
+++ src/test/java/RedirectUntilCode200Test.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RedirectUntilCode200Test {
+
+    @Test
+    public void TestLongRedirect(){
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+    }
+}
